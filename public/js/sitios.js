4051e62255534cfb062f82e9b0c8344d2713eea6
mfortea <mforteadugo@gmail.com>
2020-04-26 18:41:30 +0200

    Creado de archivo JS utiles para métodos comunes

--- public/js/sitios.js
+++ public/js/sitios.js
@@ -1,120 +1,8 @@
-//var lat = 37.6867053;
-//var lng = -5.1327931;
+// SITIOS
 
-// Variables globales
-var lat = 0;
-var lng = 0;
+// Variables globales de la sección sitios;
 var map = null;
-var estado_ubicacion = false;
 var arrayMarcadores = [];
-
-function obtenerCoordenadas() {
-    if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (pos) {
-        lat = pos.coords.latitude;
-        lng = pos.coords.longitude;
-        estado_ubicacion = true;
-        document.getElementById("estado").innerHTML = "Ubicación activada";
-        document.getElementById('cargando').style.display = "block";
-        document.getElementById('map').style.display = "block";
-        generarMapa();
-    }, function (objPositionError) {
-        // Cacheo de errores relacionados con la ubicación
-        switch (objPositionError.code) {
-            case objPositionError.PERMISSION_DENIED:
-                document.getElementById("cuerpoModal").innerHTML = "Debe permitir la ubicación para continuar";
-                document.getElementById("simboloModal").className = "fas fa-location-arrow fa-3x";
-                sonidoError();
-                MicroModal.show('modal');
-                break;
-            case objPositionError.POSITION_UNAVAILABLE:
-                document.getElementById("cuerpoModal").innerHTML = "No se ha podido acceder a la información de su posición.";
-                document.getElementById("simboloModal").className = "fas fa-location-arrow fa-3x";
-                sonidoError();
-                vibrar(300);
-                MicroModal.show('modal');
-                break;
-            case objPositionError.TIMEOUT:
-                document.getElementById("cuerpoModal").innerHTML = "El servicio ha tardado demasiado tiempo en responder.";
-                document.getElementById("simboloModal").className = "fas fa-clock fa-3x";
-                sonidoError();
-                vibrar(300);
-                MicroModal.show('modal');
-                break;
-            default:
-                document.getElementById("cuerpoModal").innerHTML = "Error desconocido";
-                document.getElementById("simboloModal").className = "fas fa-question-circle fa-3x";
-                sonidoError();
-                vibrar(300);
-                MicroModal.show('modal');
-        }
-    }, {
-        maximumAge: 75000,
-        timeout: 15000
-    });
-}
-
-
-
-function generarMapa() {
-
-    if (estado_ubicacion) {
-
-        // Creación del mapa
-        map = L.map('map').setView([lat, lng], 12);
-
-        // Crear control de pantalla completa
-        var fsControl = L.control.fullscreen();
-        map.addControl(fsControl);
-
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '',
-            subdomains: ['a', 'b', 'c']
-        }).addTo(map);
-
-        var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        var osmAttribution = 'Datos del mapa <a href="http://openstreetmap' +
-            '.org">&copy; OpenStreetMap</a>';
-        var osm = new L.TileLayer(osmUrl, { maxZoom: 18, attribution: osmAttribution });
-
-        var gUrl = 'https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}';
-        var gAttribution = '&copy; Google Maps';
-        var googlesat = new L.TileLayer(gUrl, { maxZoom: 18, attribution: gAttribution });
-
-        var baseLayers = {
-            '<i class="fas fa-map-marked-alt"></i> Callejero': osm,
-            '<i class="fas fa-globe-europe"></i> Satélite': googlesat
-        }
-
-        L.control.layers(baseLayers, {}).addTo(map);
-
-        // Círculo de la ubicación actual
-        var circle = L.circle([lat, lng], {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.2,
-            radius: 700
-        }).addTo(map);
-
-        // Icono de la ubicación actual
-        var iconoUbActual = L.icon({
-            iconUrl: 'imagenes/marcador_rojo.png',
-            iconSize: [50, 50]
-        });
-
-        // Marcador de la ubicación actual
-        var marcadorUbActual = L.marker([lat, lng], { icon: iconoUbActual }).bindPopup('<p class="tituloPopup">Tu ubicación</h2>').addTo(map);
-        arrayMarcadores.push(marcadorUbActual);
-
-        obtenerFavoritos();
-
-
-    } else {
-        document.getElementById("cuerpoModal").innerHTML = "No se ha podido determinar su ubicación";
-        document.getElementById("simboloModal").className = "fas fa-location-arrow fa-3x";
-        MicroModal.show('modal');
-    }
-}
 
 
 function obtenerFavoritos() {
@@ -163,16 +51,6 @@ function obtenerFavoritos() {
 
 }
 
-function medirDistancia(lat1, lon1, lat2, lon2) {
-    rad = function (x) { return x * Math.PI / 180; }
-    var R = 6378.137; //Radio de la tierra en km
-    var dLat = rad(lat2 - lat1);
-    var dLong = rad(lon2 - lon1);
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(lat1)) * Math.cos(rad(lat2)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    return d.toFixed(2);
-}
 
 function eliminarFavorito(id) {
 
@@ -185,10 +63,7 @@ function eliminarFavorito(id) {
             vibrar(300);
             sonidoOK()
             MicroModal.show('modal');
-            map.off();
-            map.remove();
-            console.log("Mapa eliminado");
-            obtenerCoordenadas();
+            recargarMapa();
         } else if (response.status === 404) {
             document.getElementById("cuerpoModal").innerHTML = "No se ha podido eliminar el sitio";
             document.getElementById("simboloModal").className = "fas fa-times fa-3x";
@@ -197,4 +72,13 @@ function eliminarFavorito(id) {
             MicroModal.show('modal');
         }
     })
+}
+
+function recargarMapa(){
+    while(arrayMarcadores.length > 0)
+    arrayMarcadores.pop(); 
+    map.off();
+    map.remove();
+    obtenerCoordenadas("sitios");
+
 }
